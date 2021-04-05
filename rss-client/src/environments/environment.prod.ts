0d736e917575f57eb250a5d35870a18237b87c2e
jaredmalkin <35544560+jaredmalkin@users.noreply.github.com>
2021-04-05 09:38:05 -0500

    Change aws url

--- rss-client/src/environments/environment.prod.ts
+++ rss-client/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  accountServiceUrl: 'http://a05ca03ed94f848dfa2d6ef975d395ff-284278107.us-east-1.elb.amazonaws.com/account',
-  evaluationServiceUrl: 'http://a05ca03ed94f848dfa2d6ef975d395ff-284278107.us-east-1.elb.amazonaws.com/evaluation',
-  cartServiceUrl: 'http://a05ca03ed94f848dfa2d6ef975d395ff-284278107.us-east-1.elb.amazonaws.com/cart',
-  inventoryServiceUrl: 'http://a05ca03ed94f848dfa2d6ef975d395ff-284278107.us-east-1.elb.amazonaws.com/inventory',
+  accountServiceUrl: 'http://ad8d6edfec9aa4a79be8f07ba490356a-1499412652.us-east-1.elb.amazonaws.com/account',
+  evaluationServiceUrl: 'http://ad8d6edfec9aa4a79be8f07ba490356a-1499412652.us-east-1.elb.amazonaws.com/evaluation',
+  cartServiceUrl: 'http://ad8d6edfec9aa4a79be8f07ba490356a-1499412652.us-east-1.elb.amazonaws.com/cart',
+  inventoryServiceUrl: 'http://ad8d6edfec9aa4a79be8f07ba490356a-1499412652.us-east-1.elb.amazonaws.com/inventory',
   admin: true,
 };
